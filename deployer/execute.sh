f4f404d68df0c7c4787f8763447a1ac617c64b17
Karl Hughes <khughes.me@gmail.com>
2017-04-17 10:03:47 -0500

    Minor adjustments

--- deployer/execute.sh
+++ deployer/execute.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 echo -e $PRIVATE_SSH_KEY >> $HOME/.ssh/id_rsa
-ssh -t -oStrictHostKeyChecking=no $USER@$HOST "cd docker-php-cli-example ; sh docker/deploy.sh"
+ssh -t -oStrictHostKeyChecking=no $USER@$HOST "cd docker-php-cli-example && sh docker/deploy.sh"
